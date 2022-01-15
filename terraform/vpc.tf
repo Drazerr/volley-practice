57f27e761d720aa0839c559d1e5a7659d23b4786
RaviTejaAlli <raviteja.alli@hotmail.com>
2022-01-15 09:45:27 -0600

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -10,11 +10,11 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "Terraform-eks"
 }
 
 resource "random_string" "suffix" {
-  length  = 8
+  length  = 3
   special = false
 }
 
